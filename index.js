28ec96ea0b647bc2dd08ad0e912d677b48c8fe47
tupham-ab <tu.pb@alphabravodevelopment.com>
2024-12-17 22:40:08 +0700

    Fix

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
-
 app.use(express.static("public"));
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
+
+app.listen(process.env.PORT || 3000);
